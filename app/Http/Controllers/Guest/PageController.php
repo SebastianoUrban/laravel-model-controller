66e430d5ed5754ca30180c9ba4088d8692fafe40
SebastianoUrban <sebastiano.urban98@gmail.com>
2023-02-09 16:31:11 +0100

    try to connect db

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index() {
-        return view('welcome');
+        $movies = Movie::all();
+        return view('welcome', compact('movies'));
     }
 }
